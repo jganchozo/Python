ab0ed2cdbc10e14826e9f9d9dacbba26c43ffdf6
Jose Manuel Ganchozo <jganchozo@outlook.com>
2024-07-18 09:59:21 -0400

    Initial commit

---
+++ 01-Day/Brewery.py
@@ -0,0 +1,4 @@
+favoriteColor = input("What is your favorite color: ")
+favoriteFruit = input("What is your favorite fruit: ")
+
+print(f"\nThe name of the beer is\n \"{favoriteColor} {favoriteFruit}\" \n Congratulation!")
---
+++ 01-Day/FirstProject.py
@@ -0,0 +1,4 @@
+name = input('Insert your firstname: ')
+lastName = input('Insert your lastname: ')
+
+print(f"Hello {name} {lastName}")
